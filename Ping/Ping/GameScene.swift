fcb0c049e5bb19d60c0fb59cb768deab857546d7
MondaleFelix <mondalefelix@yahoo.com>
2020-07-16 09:09:28 -0700

    added main and enemy paddle

---
+++ Ping/Ping/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  Ping
+//
+//  Created by Mondale on 7/16/20.
+//  Copyright © 2020 Mondale. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
